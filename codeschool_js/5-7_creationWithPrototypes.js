70a7a75130305445832621b1411bf81a9582a254
Bobby Joe Smith III <bobbyjoe@codeforprogress.org>
2016-04-27 17:02:15 -0500

    Add solution to codeschool javascript object prototypes challenge 5.8

--- codeschool_js/5-7_creationWithPrototypes.js
+++ codeschool_js/5-7_creationWithPrototypes.js
@@ -32,3 +32,50 @@ post2.postNum = 2;
 // connect the posts together
 post1.sendRopeTo(post2);
 post2.sendRopeTo(post1);
+
+/* CHALLENGE 5.8 - Create fence posts with special properties from object prototype genericPost */
+
+/* Three new posts :
+x: 0,
+y: -3,
+postNum: 8,
+connectionsTo: 10
+
+x: 6,
+y: 8,
+postNum: 9,
+connectionsTo: 10
+
+x: -2,
+y: 3,
+postNum: 10,
+connectionsTo: 8, 9
+*/
+
+//Post 8
+var post8 = Object.create(genericPost);
+  post8.x = 0;
+  post8.y = -3;
+  post8.postNum = 8;
+  post8.lightsOn = false;
+
+//Post 9
+var post9 = Object.create(genericPost);
+  post9.x = 6;
+  post9.y = 8;
+  post9.postNum = 9;
+  post9.numBirds = 0;
+
+//Post 10
+var post10 = Object.create(genericPost);
+  post10.x = -2;
+  post10.y = 3;
+  post10.postNum = 10;
+  post10.weathervane = "N";
+  post10.lightsOn = false;
+
+//Make post connections
+post8.sendRopeTo(post10);
+post9.sendRopeTo(post10);
+post10.sendRopeTo(post8);
+post10.sendRopeTo(post9);
