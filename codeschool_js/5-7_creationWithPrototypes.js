228de2e9c74a548d9fbe5a4279181c9665c16285
Bobby Joe Smith III <bobbyjoe@codeforprogress.org>
2016-04-27 18:34:29 -0500

    Add solution to codeschool javascript object prototypes challenge 5.11

--- codeschool_js/5-7_creationWithPrototypes.js
+++ codeschool_js/5-7_creationWithPrototypes.js
@@ -123,3 +123,60 @@ post18.sendRopeTo(post20);
 post20.sendRopeTo(post18);
 post18.sendRopeTo(post19);
 post19.sendRopeTo(post18);
+
+
+/* CHALLENGE 5.11 - Optimize constructor function for memory by putting repeated properties and methods in a prototype */
+/* Original
+function Fencepost(x, y, postNum) {
+  this.x = x;
+  this.y = y;
+  this.postNum = postNum;
+  this.connectionsTo = [];
+  this.sendRopeTo = function(connectedPost) {
+    this.connectionsTo.push(connectedPost);
+  };
+  this.removeRope = function(removeTo) {
+    var temp = [];
+    for (var i = 0; i < this.connectionsTo.length; i++) {
+      if (this.connectionsTo[i].postNum != removeTo) {
+        temp.push(this.connectionsTo[i]);
+      }
+    }
+    this.connectionsTo = temp;
+  };
+  this.movePost = function(x, y) {
+    this.x = x;
+    this.y = y;
+  };
+}
+*/
+
+function Fencepost(x, y, postNum) {
+  this.x = x;
+  this.y = y;
+  this.postNum = postNum;
+  this.connectionsTo = [];
+}
+
+//Fencepost prototype
+Fencepost.prototype = {
+  sendRopeTo: function (connectedPost) {
+    this.connectionsTo.push(connectedPost);
+  },
+
+  removeRope: function (removeTo) {
+    var temp = [];
+    for (var i = 0; i < this.connectionsTo.length; i++) {
+      if (this.connectionsTo[i].postNum != removeTo) {
+        temp.push(this.connectionsTo[i]);
+      }
+    }
+
+    this.connectionsTo = temp;
+  },
+
+  movePost: function (x, y) {
+    this.x = x;
+    this.y = y;
+  }
+};
