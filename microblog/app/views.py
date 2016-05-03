1ad391fb3f0e22a7b5bd8267272ca8b4888749c0
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2016-05-03 11:22:45 -0500

    Recreate example using templates. Add index.html file

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,6 +1,8 @@
 '''
 The views are the handlers that respond to requests from web browsers or other clients. In Flask handlers are written as Python functions. Each view function is mapped to one or more request URLs.
 '''
+#import render_template method
+from flask import render_template
 from app import app
 
 '''
@@ -10,13 +12,6 @@ The two route decorators above the function create the mappings from URLs '/' an
 @app.route('/index')
 def index():
   user = {'nickname': 'Miguel'} #fake user
-  return '''
-  <html>
-    <head>
-      <title>Home Page</title>
-    </head>
-    <body>
-      <h1>Hello, ''' + user['nickname'] + '''</h1>
-    </body>
-  </html>
-  '''
+  return render_template('index.html',
+                          title='Home',
+                          user=user)
