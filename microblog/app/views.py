1ccaec00c8ef4dbedf97e55ee16f8142bd2b09c0
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2016-05-03 12:47:46 -0500

    Add login page with openId forms. Route page through views.

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -2,12 +2,17 @@
 The views are the handlers that respond to requests from web browsers or other clients. In Flask handlers are written as Python functions. Each view function is mapped to one or more request URLs.
 '''
 #import render_template method invoking jinja2 templating engine
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+#import LoginForm() class in forms.py
+from .forms import LoginForm
 
-'''
-The two route decorators above the function create the mappings from URLs '/' and '/index' to the function returning Hello, World!
-'''
+
+'''==============================================
+          INDEX
+=============================================='''
+#The two route decorators above the function create the mappings from URLs '/' and '/index' to the function returning Hello, World!
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -26,3 +31,16 @@ def index():
   return render_template('index.html',
                           user=user,
                           posts=posts)
+
+
+'''==============================================
+          LOGIN
+=============================================='''
+#Import LoginForm class (done above), instantiate an object from it, and send it down to the template
+#Methods argument tells Flask that this view function accepts GET and POST requests. Without this, the view will only accept GET requests.
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  form = LoginForm()
+  return render_template('login.html',
+                          title='Sign In',
+                          form=form)
