6e3d22986573fbcfa75a1b0daab8ce9551698f31
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2016-05-03 11:12:44 -0500

    Add example of how we would have to generate page templates if we didn't use templates in our application

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,7 +1,22 @@
-'''The views are the handlers that respond to requests from web browsers or other clients. In Flask handlers are written as Python functions. Each view function is mapped to one or more request URLs.'''
+'''
+The views are the handlers that respond to requests from web browsers or other clients. In Flask handlers are written as Python functions. Each view function is mapped to one or more request URLs.
+'''
 from app import app
 
+'''
+The two route decorators above the function create the mappings from URLs '/' and '/index' to the function returning Hello, World!
+'''
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!"
+  user = {'nickname': 'Miguel'} #fake user
+  return '''
+  <html>
+    <head>
+      <title>Home Page</title>
+    </head>
+    <body>
+      <h1>Hello, ''' + user['nickname'] + '''</h1>
+    </body>
+  </html>
+  '''
