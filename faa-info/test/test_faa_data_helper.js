71f0a7f45fef54f3dfb9bb89b714e70ddedb6573
Bobby Joe Smith III <bobbyjoe@codeforprogress.org>
2016-08-08 12:07:25 -0500

    Build airport information request and test that it works. Responds as expected with both a valid and invalid airport code.

--- faa-info/test/test_faa_data_helper.js
+++ faa-info/test/test_faa_data_helper.js
@@ -1,14 +1,4 @@
 'use strict';
-var _ = require('lodash');
-var rp = require('request-promise');
-var ENDPOINT = 'http://services.faa.gov/airport/status/';
-
-function FAADataHelper() { }
-
-FAADataHelper.prototype.requestAirportStatus = function(airportCode) {
-};
-
-module.exports = FAADataHelper;
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
@@ -20,12 +10,21 @@ describe('FAADataHelper', function() {
   var subject = new FAADataHelper();
   var airport_code;
   describe('#getAirportStatus', function() {
+    context('with an invalid airport code', function() {
+      it('returns an error', function() {
+        airport_code = 'PUNKYBREWSTER';
+        return expect(subject.requestAirportStatus(airport_code)).to.be.rejectedWith(Error);
+      });
+    });
     context('with a valid airport code', function() {
       it('returns matching airport code', function() {
         airport_code = 'SFO';
         var value = subject.requestAirportStatus(airport_code).then(function(obj) {
           return obj.IATA;
         });
+        /*
+        This is a special part of the chai-as-promised matcher we added, which allows us to make assertions about the data a Promise returns without requiring the use of callbacks, “sleeps” or other less-than-ideal approaches to waiting for the request to complete.
+        */
         return expect(value).to.eventually.eq(airport_code);
       });
     });
