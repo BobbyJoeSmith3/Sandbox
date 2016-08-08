35c050f7018d7dafe8ab07f66d275184375a5a28
Bobby Joe Smith III <bobbyjoe@codeforprogress.org>
2016-08-08 10:46:23 -0500

    first commit for an alexa skill build utilizing airport info to track airport flight delays and weather conditions.

---
+++ faa-info/test/test_faa_data_helper.js
@@ -0,0 +1,33 @@
+'use strict';
+var _ = require('lodash');
+var rp = require('request-promise');
+var ENDPOINT = 'http://services.faa.gov/airport/status/';
+
+function FAADataHelper() { }
+
+FAADataHelper.prototype.requestAirportStatus = function(airportCode) {
+};
+
+module.exports = FAADataHelper;
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+var FAADataHelper = require('../faa_data_helper');
+chai.config.includeStack = true;
+
+describe('FAADataHelper', function() {
+  var subject = new FAADataHelper();
+  var airport_code;
+  describe('#getAirportStatus', function() {
+    context('with a valid airport code', function() {
+      it('returns matching airport code', function() {
+        airport_code = 'SFO';
+        var value = subject.requestAirportStatus(airport_code).then(function(obj) {
+          return obj.IATA;
+        });
+        return expect(value).to.eventually.eq(airport_code);
+      });
+    });
+  });
+});
